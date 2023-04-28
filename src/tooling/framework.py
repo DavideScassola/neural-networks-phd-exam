e2134a7b9a234859e66982962294554daf5658bc
dbasso98 <dav.basso98@gmail.com>
2023-04-28 08:33:00 +0200

    framework.py changes

--- src/tooling/framework.py
+++ src/tooling/framework.py
@@ -43,6 +43,43 @@ def teacher_dataset(
     return DataLoader(list(zip(input_data, first_labels, second_labels)))
 
 
+def student_training_two_teachers(
+    student: 'TwoHeadStudent', train_data: 'DataLoader',
+):
+
+    optimizer = th.optim.SGD(student.trainable_parameters(lr=1), lr=1)
+    loss_fn = nn.MSELoss()
+
+    # Metrics
+    running_loss = []
+
+    student.train()
+    for index, data in enumerate(train_data):
+        vec, first_label, second_label = data
+
+        optimizer.zero_grad()
+
+        if index == len(train_data.dataset)//2:
+            student.flip_switch()
+
+        out_1, out_2 = student(vec, return_both_heads=True)
+
+        if index < len(train_data.dataset)//2:
+            loss = loss_fn(out_1, first_label.view((1,1)).detach())
+        else:
+            loss = loss_fn(out_2, second_label.view((1,1)).detach())
+
+        loss.backward()
+
+        optimizer.step()
+
+        running_loss.append(loss.item())
+
+    avg_loss = sum(running_loss)/len(train_data.dataset)
+        
+    return running_loss, avg_loss
+
+
 def student_training(
     student: 'TwoHeadStudent', train_data: 'DataLoader',
 ):
@@ -57,7 +94,7 @@ def student_training(
     for data, label in train_data:
         optimizer.zero_grad()
         out = student(data, return_both_heads=False)
-        loss = loss_fn(out, label.detach())
+        loss = loss_fn(out, label.view((1,1)).detach())
         loss.backward()
         optimizer.step()
 
@@ -69,12 +106,13 @@ def student_training(
 
 
 if __name__ == '__main__':
-    teacher = arc.goldt_school(1)
-    student = arc.goldt_student(1)
-    teacher_data = teacher_input_data(5)
-    teacher_labels = teacher(teacher_data, False)
-    noise = th.randn(teacher_labels.size())
-    teacher_labels += noise
-    student_training_data = DataLoader(list(zip(teacher_data, teacher_labels)))
+    # teacher = arc.goldt_school(1)
+    # student = arc.goldt_student(1)
+    # teacher_data = teacher_input_data(5)
+    # teacher_labels = teacher(teacher_data, False)
+    # noise = th.randn(teacher_labels.size())
+    # teacher_labels += noise
+    # student_training_data = DataLoader(list(zip(teacher_data, teacher_labels)))
 
-    dataset = teacher_dataset(5, th.tensor(0.5))
+    dataset = teacher_dataset(100, th.tensor(0.))
+    generalization_err, avg_loss = student_training_two_teachers(arc.goldt_student(1), dataset)
